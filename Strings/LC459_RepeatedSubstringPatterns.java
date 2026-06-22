0bd7c27ceffb0616a7eb8499651464f5de830800
Akshat-Tated <akshattated18@gmail.com>
2026-06-22 21:47:14 +0530

    Day-3 String Solutions

---
+++ Strings/LC459_RepeatedSubstringPatterns.java
@@ -0,0 +1,24 @@
+package Strings;
+
+public class LC459_RepeatedSubstringPatterns {
+    public static boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int len = 1; len <= n / 2; len++) {
+            if (n % len != 0) continue;
+            String sub = s.substring(0, len);
+            StringBuilder sb = new StringBuilder();
+            int times = n / len;
+            for (int i = 0; i < times; i++) {
+                sb.append(sub);
+            }
+            if (sb.toString().equals(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String s = "abab";
+        System.out.println(repeatedSubstringPattern(s));
+    }
+}
